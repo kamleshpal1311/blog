6008a8ffa524ed1bb776ef4176d59577d71e9874
kamleshpal1311 <kamlesh.pal@openxcellinc.com>
2016-08-27 16:04:48 +0530

    all changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'glickin');
+define('DB_NAME', 'database_name');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'user');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'just@123');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
